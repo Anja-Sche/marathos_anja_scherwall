304103b7b44f18bfd212f40d441b40443e34b41a
Anja-Sche <anjagutt@hotmail.com>
2026-05-25 15:57:08 +0000

    Load streaming csv data into bronze layer

---
+++ transformations/bronze/raw_country_reference.py
@@ -0,0 +1,21 @@
+from pyspark import pipelines as dp 
+
+BASE_DIR = "/Volumes/marathos/default/raw"
+
+schema = (
+    spark.read.format("csv")
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .load(f"{BASE_DIR}/country_data/country_codes_reference.csv")
+    .schema
+)
+
+@dp.table(name = "marathos.bronze.raw_country_reference", 
+          comment = "Raw country reference data for connecting shortening of countries to the country - bronze layer", 
+          table_properties = {
+            "delta.columnMapping.mode": "name",
+           "delta.minReaderVersion": "2",
+           "delta.minWriterVersion": "5"
+          })
+def raw_country_reference():
+    return spark.readStream.format("csv").options(header  =True, encoding = "UTF-8").schema(schema).load(f"{BASE_DIR}/country_data")
---
+++ transformations/bronze/raw_ultra_marathon.py
@@ -0,0 +1,21 @@
+from pyspark import pipelines as dp 
+
+BASE_DIR = "/Volumes/marathos/default/raw"
+
+schema = (
+    spark.read.format("csv")
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .load(f"{BASE_DIR}/data/TWO_CENTURIES_OF_UM_RACES.csv")
+    .schema
+)
+
+@dp.table(name = "marathos.bronze.raw_ultra_marathon", 
+          comment = "Raw ultra marathon data for Marathos - bronze layer", 
+          table_properties = {
+            "delta.columnMapping.mode": "name",
+           "delta.minReaderVersion": "2",
+           "delta.minWriterVersion": "5"
+          })
+def raw_ultra_marathon():
+    return spark.readStream.format("csv").options(header  =True, encoding = "UTF-8").schema(schema).load(f"{BASE_DIR}/data")
