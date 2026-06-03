b553fa8c5a4613f461ac3f3554e6068fa48726bd
Anja-Sche <anjagutt@hotmail.com>
2026-06-03 04:41:01 +0000

    Update mart for dashboard

--- transformations/gold/mart_athlete_year.sql
+++ transformations/gold/mart_athlete_year.sql
@@ -5,10 +5,9 @@ CREATE OR REFRESH MATERIALIZED VIEW marathos.gold.mart_athlete_year
 COMMENT "Mart for information about athletes, gender, birth year and year they competed - gold layer" AS
 
 SELECT
-    d.year_of_event AS event_year,
+    a.athlete_id,
     a.year_of_birth AS birth_year,
     a.athlete_gender,
-    r.athlete_average_speed,
     d.year_of_event - a.year_of_birth AS athlete_age
 FROM
     fct_result r
@@ -17,8 +16,8 @@ LEFT JOIN dim_date d ON r.date_id = d.date_id
 WHERE 
     (d.year_of_event - a.year_of_birth) >= 10 AND (d.year_of_event - a.year_of_birth) <= 95
 GROUP BY 
-    event_year,
+    a.athlete_id,
     birth_year,
     a.athlete_gender,
-    r.athlete_average_speed
-ORDER BY event_year ASC;
+    d.year_of_event
+ORDER BY birth_year ASC;
