048401c56ee1c5091dc6b319fe73e9cafa6e179d
Anja-Sche <anjagutt@hotmail.com>
2026-06-03 12:54:45 +0000

    Create athlete_id and update date filter for future data
    When taking a quick look att the LLM generated data I saw two things I had to update. First was to create a new athlete ID so that new values would not crash and the other one was tu turn "-" to "." when date is only one day.

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,6 @@
 import re
 from pyspark.sql import functions as sf, DataFrame
-from pyspark.sql.functions import col, coalesce, lit
+from pyspark.sql.functions import col, coalesce, lit, when, lower, upper
 from pyspark.sql.types import StringType
 from functools import reduce
 
@@ -21,21 +21,21 @@ def rename_columns_to_snake_case(df):
 def remove_unclean_dl_performance(df):
     return df.filter(
         (
-            sf.lower(sf.col("event_distance_length")).rlike("(km|mi)$")
-            & sf.lower(sf.col("athlete_performance")).rlike("h$")
-            & ~sf.lower(sf.col("athlete_performance")).rlike("d")
+            lower(col("event_distance_length")).rlike("(km|mi)$")
+            & lower(col("athlete_performance")).rlike("h$")
+            & ~lower(col("athlete_performance")).rlike("d")
         )
         | (
-            sf.lower(sf.col("event_distance_length")).rlike("h$")
-            & sf.lower(sf.col("athlete_performance")).rlike("km$")
+            lower(col("event_distance_length")).rlike("h$")
+            & lower(col("athlete_performance")).rlike("km$")
         )
         | (
-            sf.lower(sf.col("athlete_performance")).rlike("km$")
-            & sf.lower(sf.col("event_distance_length")).rlike("h$")
+            lower(col("athlete_performance")).rlike("km$")
+            & lower(col("event_distance_length")).rlike("h$")
         )
         | (
-            sf.lower(sf.col("athlete_performance")).rlike("h$")
-            & sf.lower(sf.col("event_distance_length")).rlike("(km|mi)$")
+            lower(col("athlete_performance")).rlike("h$")
+            & lower(col("event_distance_length")).rlike("(km|mi)$")
         )
     )
 
@@ -52,17 +52,17 @@ def clean_event_distance_length(df):
         .withColumn(
             "distance_length_value",
             sf.round(
-                sf.when(
-                    sf.col("distance_length_type") == "mi",
+                when(
+                    col("distance_length_type") == "mi",
                     sf.expr("try_cast(distance_length_value as double)") * 1.609344,
                 ).otherwise(sf.expr("try_cast(distance_length_value as double)")),
                 2,))
         .withColumn(
             "distance_length_type",
-            sf.when(sf.col("distance_length_type") == "mi", sf.lit("km")).otherwise(
-                sf.col("distance_length_type")
+            when(col("distance_length_type") == "mi", lit("km")).otherwise(
+                col("distance_length_type")
             ))
-        .filter(sf.col("distance_length_value").isNotNull())
+        .filter(col("distance_length_value").isNotNull())
     )
     return df
 
@@ -70,9 +70,9 @@ def clean_event_distance_length(df):
 
 def distance_filter(df):
     df = df.filter(
-            ((sf.col("distance_length_type") == "km") &
-            (sf.col("distance_length_value") > 42)) |
-        (sf.col("distance_length_type") == "h")
+            ((col("distance_length_type") == "km") &
+            (col("distance_length_value") > 42)) |
+        (col("distance_length_type") == "h")
     )
     return df
 
@@ -90,8 +90,8 @@ def calculate_performance_h(df):
         )
         .withColumn(
             "performance_value",
-            sf.when(
-                sf.col("performance_value").contains(":"),
+            when(
+                col("performance_value").contains(":"),
                 sf.round(((
                             sf.expr(
                                 "try_cast(substring_index(performance_value, ':', 1) as int)")* 3600
@@ -102,7 +102,7 @@ def calculate_performance_h(df):
                             )/ 3600),5,).cast("double"),
             ).otherwise(sf.expr("try_cast(performance_value as double)"))
         )
-    ).filter(sf.col("performance_value").isNotNull())
+    ).filter(col("performance_value").isNotNull())
 
     return df
 
@@ -122,37 +122,43 @@ def extract_country_code_event_name(df):
 
 def split_dates(df):
     df = df.withColumn(
+        "event_dates",
+        when(
+            (sf.length(col("event_dates")) == 10) & col("event_dates").contains("-"),
+            sf.regexp_replace(col("event_dates"), "-", ".")
+        ).otherwise(col("event_dates"))
+    ).withColumn(
     "start_date",
-    sf.when(
-        sf.col("event_dates").rlike(r"^\d{2}\.-\d{2}\."),
+    when(
+        col("event_dates").rlike(r"^\d{2}\.-\d{2}\."),
         sf.concat(
-            sf.substring(sf.col("event_dates"), 1, 3),
-            sf.substring(sf.col("event_dates"), 8, 8)
+            sf.substring(col("event_dates"), 1, 3),
+            sf.substring(col("event_dates"), 8, 8)
         )
     ).when(
-        sf.col("event_dates").rlike(r"^\d{2}\.\d{2}\.\-"),
+        col("event_dates").rlike(r"^\d{2}\.\d{2}\.\-"),
         sf.concat(
-            sf.substring(sf.col("event_dates"), 1, 6),  
-            sf.substring(sf.col("event_dates"), -4, 4)  
+            sf.substring(col("event_dates"), 1, 6),  
+            sf.substring(col("event_dates"), -4, 4)  
         )
     ).otherwise(
-        sf.when(
-            sf.col("event_dates").contains("-"),
-            sf.split(sf.col("event_dates"), "-").getItem(0)
-        ).otherwise(sf.substring(sf.col("event_dates"), 1, 10))
+        when(
+            col("event_dates").contains("-"),
+            sf.split(col("event_dates"), "-").getItem(0)
+        ).otherwise(sf.substring(col("event_dates"), 1, 10))
     )
     ).withColumn(
         "start_date",
-        sf.try_to_date(sf.col("start_date"), "dd.MM.yyyy")
+        sf.try_to_date(col("start_date"), "dd.MM.yyyy")
     ).withColumn(
         "end_date",
-        sf.when(
-            sf.col("event_dates").contains("-"),
-            sf.regexp_extract(sf.col("event_dates"), r"\-([0-9,.;:]+)\s*", 1)
-        ).otherwise(sf.col("event_dates"))
+        when(
+            col("event_dates").contains("-"),
+            sf.regexp_extract(col("event_dates"), r"\-([0-9,.;:]+)\s*", 1)
+        ).otherwise(col("event_dates"))
     ).withColumn(
         "end_date",
-        sf.try_to_date(sf.col("end_date"), "dd.MM.yyyy")
+        sf.try_to_date(col("end_date"), "dd.MM.yyyy")
     ).dropna(subset=["start_date", "end_date"])
     return df
 
@@ -161,24 +167,24 @@ def split_dates(df):
 def calculate_age(df):
     df = df.withColumn(
         "athlete_age",
-        sf.col("year_of_event") - sf.col("athlete_year_of_birth").cast("int")
+        col("year_of_event") - col("athlete_year_of_birth").cast("int")
     )
     return df
 
 def valid_age(df):
     age_filter = (
-        (sf.col("athlete_age") >= 10) & 
-        (sf.col("athlete_age") <= 95) 
+        (col("athlete_age") >= 10) & 
+        (col("athlete_age") <= 95) 
     )
     return df.filter(age_filter).drop("athlete_age")
 
 """Validate that gender and age category match, standardize gender name"""
 
 def validate_gender_category(df):
-    extracted_letter = sf.regexp_extract(sf.col("athlete_age_category"), r"(^[a-zA-Z]+)\s*", 1)
+    extracted_letter = sf.regexp_extract(col("athlete_age_category"), r"(^[a-zA-Z]+)\s*", 1)
     
-    valid_male = (sf.col("athlete_gender") == "M") & (extracted_letter.isin("M", "MU"))
-    vaild_female = (sf.col("athlete_gender") == "F") & (extracted_letter.isin("F", "FU", "W", "WU"))
+    valid_male = (col("athlete_gender") == "M") & (extracted_letter.isin("M", "MU"))
+    vaild_female = (col("athlete_gender") == "F") & (extracted_letter.isin("F", "FU", "W", "WU"))
 
     df = df.filter(valid_male | vaild_female)
         
@@ -187,35 +193,35 @@ def validate_gender_category(df):
 def standardize_gender_name(df):
     return df.withColumn(
         "athlete_gender",
-        sf.when(sf.col("athlete_gender") == "M", sf.lit("Male"))
-        .otherwise(sf.lit("Female"))
+        when(col("athlete_gender") == "M", lit("Male"))
+        .otherwise(lit("Female"))
     )
 
 """Calculate athlete avg speed"""
 
 def calculate_avg_speed(df):
     df = df.filter(
-        (sf.col("performance_value") > 0) & (sf.col("distance_length_value") > 0)
+        (col("performance_value") > 0) & (col("distance_length_value") > 0)
         ).withColumn(
             "athlete_avg_speed",
             sf.round(
-                sf.when(
-                (sf.col("distance_length_type") == "km"),
+                when(
+                (col("distance_length_type") == "km"),
                 sf.try_divide(
-                    sf.col("distance_length_value"),
-                    sf.col("performance_value")) 
+                    col("distance_length_value"),
+                    col("performance_value")) 
             ).when(
-                (sf.col("distance_length_type") == "h"),
+                (col("distance_length_type") == "h"),
                 sf.try_divide(
-                    sf.col("performance_value"),
-                    sf.col("distance_length_value")) 
-            ).otherwise(sf.lit(None)), 3 
+                    col("performance_value"),
+                    col("distance_length_value")) 
+            ).otherwise(lit(None)), 3 
         )
     )
     return df
 
 def valid_avg_speed(df):
-    valid_speed = sf.col("athlete_avg_speed") <= 20
+    valid_speed = col("athlete_avg_speed") <= 20
     return df.filter(valid_speed)
 
 """Join country code/name tables and replace country codes to names for event and athlete"""
@@ -223,10 +229,10 @@ def valid_avg_speed(df):
 def create_country_and_code(df_country, df_country_old):
     def clean_match(df, code_col):
         return (
-            df.select("Country", sf.upper(sf.col(code_col)).alias("code"))
+            df.select("Country", upper(col(code_col)).alias("code"))
             .filter(
-                (sf.col("Country").isNotNull()) & (sf.col("Country") != "NA") &
-                (sf.col("code").isNotNull()) & (sf.col("code") != "NA")
+                (col("Country").isNotNull()) & (col("Country") != "NA") &
+                (col("code").isNotNull()) & (col("code") != "NA")
             )
         )
     dfs = [
@@ -242,7 +248,7 @@ def create_country_and_code(df_country, df_country_old):
 def join_country_name(df, df_countries_and_codes, country_code_col, new_col_name):
     df = df.join(
         df_countries_and_codes, 
-        sf.col(country_code_col) == df_countries_and_codes["code"], 
+        col(country_code_col) == df_countries_and_codes["code"], 
         how="left"
     )
     df = df.withColumnRenamed("Country", new_col_name)
@@ -254,6 +260,6 @@ def join_country_name(df, df_countries_and_codes, country_code_col, new_col_name
 def null_to_unknown_strings_types(df):
     for field in df.schema.fields:
         if isinstance(field.dataType, StringType):
-            df = df.withColumn(field.name, sf.coalesce(sf.col(field.name), sf.lit("unknown")))
+            df = df.withColumn(field.name, coalesce(col(field.name), lit("unknown")))
             
     return df
